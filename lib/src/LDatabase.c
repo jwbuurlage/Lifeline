a93c69efce0bf1b804ef0a7b26d3cb72b17a25ac
Tom Bannink <tombannink@gmail.com>
2012-08-26 14:12:34 +0200

    LDatabase keeps list of buffers

--- lib/src/LDatabase.c
+++ lib/src/LDatabase.c
@@ -1,16 +1,38 @@
 #include "../include/LDatabase.h"
 
+#define MAX_BUFFERS 16
+
+void* symbolbuffers[MAX_BUFFERS];
+int bufCount = 0;
+
 //
 // The application is supposed to load the files in memory and then pass a pointer to this memory
 // On failure, this function returns zero and the memory should be freed by the caller
 // On success, this function returns non-zero and the memory block will be used by the library and may
-//   not be freed untill database_freePointer is called
+//   not be freed untill database_free_pointer is called
+//   After database_free_pointer is called the caller should free the memory
 //
 int database_add_pointer(void* fileData){
+	if( bufCount == MAX_BUFFERS ) return 0;
+	symbolbuffers[bufCount] = fileData;
+	++bufCount;
 	return 1;
 }
 
 void database_free_pointer(void* fileData){
+	int i;
+	for( i = 0; i < bufCount; ++i ){
+		if( symbolbuffers[i] == fileData ) break;
+	}
+	if( i == bufCount ) return; //was not in list
+
+	//Shift the other entries one position downwards
+	--bufCount;
+	while(i < bufCount){
+		symbolbuffers[i] = symbolbuffers[i+1];
+		++i;
+	}
+	symbolbuffers[bufCount] = 0;
 	return;
 }
 
