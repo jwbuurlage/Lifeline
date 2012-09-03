988868044f45ad7acd7e54012f98c4488ea15198
jwbuurlage <jwbuurlage@me.com>
2012-09-03 14:55:10 +0200

    started documenting LRecognizer

--- lib/include/LRecognizer.h
+++ lib/include/LRecognizer.h
@@ -38,21 +38,38 @@ LPoint* points_center(LPointData* pointData);
 
 ///////////////////////////////////////////////////////////////////////////////
 
-// Initialize character data from files
+/*!
+ * Initialize character data from file, and sets up the recognizer to use it.
+ */
 void recognizer_load_data(LRecognizer *recog);
 
-// Incoming data
+/*!
+ * Set the client data set to test against. The recognizer will proceed to
+ * process the data and match the character against the database.
+ */
 void recognizer_set_data(LRecognizer *recog, LPointData* pointData);
+
+/*!
+ * Normalizes the data by sending the smallest square that fits the data to the
+ * unit square [-1, 1] x [-1, 1].
+ */
+void recognizer_normalize_data(LRecognizer *recog);
+
+/*!
+ * Connects the data, such that there are no gaps in a single stroke. The time
+ * offset for a new stroke is 0.1 seconds.
+ */
+void recognizer_connect_data(LRecognizer *recog);
+
+/*!
+ * Creates an LImage from the source_data and saves it in recog.source_image
+ */
+void recognizer_create_image(LRecognizer *recog);
+
+///////////////////////////////////////////////////////////////////////////////
 
 void recognizer_clear_samples(LRecognizer *recog);
 void recognizer_take_sample(LRecognizer *recog);
 void recognizer_save_samples(LRecognizer *recog, char* symbol);
-
-// Preprocessing
-void recognizer_normalize_data(LRecognizer *recog);
-void recognizer_connect_data(LRecognizer *recog);
-void recognizer_create_image(LRecognizer *recog);
-
-///////////////////////////////////////////////////////////////////////////////
 
 #endif
